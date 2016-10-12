cc4881ef0c70647e9a8ce34a54bc4c88846aab03
Matt Lane <lane.matthew@gmail.com>
2016-10-11 20:27:51 -0700

    add patterns 3 and 4

---
+++ pattern3.js
@@ -0,0 +1,3 @@
+function patternTest(arr) {
+  return Math.abs(+arr[0] + (+arr[1]) - (+arr[2])) < 1e-13;
+} 
---
+++ pattern4.js
@@ -0,0 +1,11 @@
+function isPrime(n) {
+  if (n < 2 || n % 1 !== 0) return false;
+  for (var i = 2; i < Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+function patternTest(arr) {
+  return arr.every(isPrime);
+} 
